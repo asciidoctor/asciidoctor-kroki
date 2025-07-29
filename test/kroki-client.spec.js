80314622749ddf7a3e7cbb3ce4dbe0d7d9d09924
Guillaume Grossetie <ggrossetie@gmail.com>
2025-07-29 17:19:22 +0200

    chore: test against a local container (#476)

--- test/kroki-client.spec.js
+++ test/kroki-client.spec.js
@@ -1,5 +1,7 @@
-/* global describe it */
+/* global describe it before after */
 // @ts-check
+const { GenericContainer } = require('testcontainers')
+const os = require('node:os')
 const chai = require('chai')
 const expect = chai.expect
 const dirtyChai = require('dirty-chai')
@@ -8,7 +10,10 @@ chai.use(dirtyChai)
 const { readFixture } = require('./utils.js')
 const { KrokiClient, KrokiDiagram } = require('../src/kroki-client.js')
 const httpClient = require('../src/http/node-http.js')
+
 const asciidoctor = require('@asciidoctor/core')()
+let container
+let krokiServerUrl
 
 describe('Kroki HTTP client', function () {
   this.timeout(30000)
@@ -64,63 +69,77 @@ describe('Kroki HTTP client', function () {
     })
   })
   describe('Adaptive mode', () => {
-    it('should get an image with GET request if the URI length is <= 4000', () => {
-      const doc = asciidoctor.load('')
-      const krokiClient = new KrokiClient(doc, httpClient)
-      const krokiDiagram = new KrokiDiagram('vegalite', 'svg', readFixture('chart.vlite'), {})
-      const image = krokiClient.getImage(krokiDiagram)
-        .replace(/\r/, '')
-        .replace(/\n/, '')
-      const expected = readFixture('expected', 'chart.svg')
-        .replace(/\r/, '')
-        .replace(/\n/, '')
-      expect(image).to.equal(expected)
-    })
-    it('should get an image with POST request if the URI length is > 4000', () => {
-      const doc = asciidoctor.load('')
-      const krokiClient = new KrokiClient(doc, httpClient)
-      const krokiDiagram = new KrokiDiagram('vegalite', 'svg', readFixture('cars-repeated-charts.vlite'), {})
-      const image = krokiClient.getImage(krokiDiagram)
-        .replace(/\r/, '')
-        .replace(/\n/, '')
-      const expected = readFixture('expected', 'cars-repeated-charts.svg')
-        .replace(/\r/, '')
-        .replace(/\n/, '')
-      expect(image).to.equal(expected)
-    })
-    it('should get an image with POST request if the URI length is greater than the value configured', () => {
-      const doc = asciidoctor.load('')
-      doc.setAttribute('kroki-max-uri-length', '10')
-      const krokiClient = new KrokiClient(doc, {
-        get: (uri) => `GET ${uri}`,
-        post: (uri, body) => `POST ${uri} - ${body}`
+    if (os.platform() !== 'win32') {
+      before(async function () {
+        this.timeout(60000)
+        container = await new GenericContainer('yuzutech/kroki:0.28.0')
+          .withExposedPorts(8000)
+          .start()
+        krokiServerUrl = `http://${container.getHost()}:${container.getMappedPort(8000)}`
       })
-      const krokiDiagram = {
-        type: 'type',
-        format: 'format',
-        text: 'text',
-        opts: {},
-        getDiagramUri: () => 'diagram-uri' // length: 11
-      }
-      const image = krokiClient.getImage(krokiDiagram)
-      expect(image).to.equal('POST https://kroki.io/type/format - text')
-    })
-    it('should get an image with GET request if the URI length is lower or equals than the value configured', () => {
-      const doc = asciidoctor.load('')
-      doc.setAttribute('kroki-max-uri-length', '11')
-      const krokiClient = new KrokiClient(doc, {
-        get: (uri) => `GET ${uri}`,
-        post: (uri, body) => `POST ${uri} - ${body}`
+
+      after(async function () {
+        this.timeout(60000)
+        await container.stop()
       })
-      const krokiDiagram = {
-        type: 'type',
-        format: 'format',
-        text: 'text',
-        opts: {},
-        getDiagramUri: () => 'diagram-uri' // length: 11
-      }
-      const image = krokiClient.getImage(krokiDiagram)
-      expect(image).to.equal('GET diagram-uri')
-    })
+      it('should get an image with GET request if the URI length is <= 4000', () => {
+        const doc = asciidoctor.load('', { attributes: { 'kroki-server-url': krokiServerUrl } })
+        const krokiClient = new KrokiClient(doc, httpClient)
+        const krokiDiagram = new KrokiDiagram('vegalite', 'svg', readFixture('chart.vlite'), {})
+        const image = krokiClient.getImage(krokiDiagram)
+          .replace(/\r/, '')
+          .replace(/\n/, '')
+        const expected = readFixture('expected', 'chart.svg')
+          .replace(/\r/, '')
+          .replace(/\n/, '')
+        expect(image).to.equal(expected)
+      })
+      it('should get an image with POST request if the URI length is > 4000', () => {
+        const doc = asciidoctor.load('', { attributes: { 'kroki-server-url': krokiServerUrl } })
+        const krokiClient = new KrokiClient(doc, httpClient)
+        const krokiDiagram = new KrokiDiagram('vegalite', 'svg', readFixture('cars-repeated-charts.vlite'), {})
+        const image = krokiClient.getImage(krokiDiagram)
+          .replace(/\r/, '')
+          .replace(/\n/, '')
+        const expected = readFixture('expected', 'cars-repeated-charts.svg')
+          .replace(/\r/, '')
+          .replace(/\n/, '')
+        expect(image).to.equal(expected)
+      })
+      it('should get an image with POST request if the URI length is greater than the value configured', () => {
+        const doc = asciidoctor.load('')
+        doc.setAttribute('kroki-max-uri-length', '10')
+        const krokiClient = new KrokiClient(doc, {
+          get: (uri) => `GET ${uri}`,
+          post: (uri, body) => `POST ${uri} - ${body}`
+        })
+        const krokiDiagram = {
+          type: 'type',
+          format: 'format',
+          text: 'text',
+          opts: {},
+          getDiagramUri: () => 'diagram-uri' // length: 11
+        }
+        const image = krokiClient.getImage(krokiDiagram)
+        expect(image).to.equal('POST https://kroki.io/type/format - text')
+      })
+      it('should get an image with GET request if the URI length is lower or equals than the value configured', () => {
+        const doc = asciidoctor.load('')
+        doc.setAttribute('kroki-max-uri-length', '11')
+        const krokiClient = new KrokiClient(doc, {
+          get: (uri) => `GET ${uri}`,
+          post: (uri, body) => `POST ${uri} - ${body}`
+        })
+        const krokiDiagram = {
+          type: 'type',
+          format: 'format',
+          text: 'text',
+          opts: {},
+          getDiagramUri: () => 'diagram-uri' // length: 11
+        }
+        const image = krokiClient.getImage(krokiDiagram)
+        expect(image).to.equal('GET diagram-uri')
+      })
+    }
   })
 })
